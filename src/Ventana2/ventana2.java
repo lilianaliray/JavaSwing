fbdf4bcc338922157bae4af7e0a6be63bc20f15b
lilianaliray <marcelo_masoka@hotmail.com>
2017-11-08 21:14:51 -0300

    paso 7-propiedades de los objetos

---
+++ src/Ventana2/ventana2.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ventana2;
+//pasi 1- importamos toda la libreria
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author LAB04
+ */
+public class  ventana2 extends JFrame
+{
+    // paso 3- declarar los elementos a utilizar
+    JPanel panel;
+    JLabel label;
+    JTextField text;
+    JButton boton;
+    JCheckBox ticket;
+    JRadioButton elegir;
+    
+    
+     //paso 4-definir el constructor
+    public ventana2()
+    {
+        //paso 5- crear inicializa los objetos
+       panel  = new JPanel();// creo un objeto tipo JPanel
+       label = new JLabel();
+       text = new JTextField(20);//20 es la cantidad de caracteres
+       boton = new JButton();
+       ticket = new JCheckBox();
+       elegir = new JRadioButton();
+               
+       
+       // paso 6- agregar los elementos al JFrame
+       this.add(panel);
+       panel.add(label);
+       panel.add(text);
+       panel.add(boton);
+       
+       
+       //paso 7-propiedades de los objetos
+       
+       label.setText("Nombre");
+       boton.setText("Enviar");
+       panel.setBackground(Color.pink);//color de fondo del JPanel
+    }
+   
+    {
+        
+    }
+    
+}
---
+++ src/javaswing/JavaSwing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaswing;
+// paso 8- importar la clase con la ventana
+import Ventana2.Ventana2;// import paquete.clases
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author LAB04
+ */
+public class JavaSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
